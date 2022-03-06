3cb1be4495a0cc96fb4c945506eeea2900642a04
xiaohh <1296522554@qq.com>
2022-03-07 01:26:49 +0800

    可以加载scss文件了

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import "./css/style.css"
 import "./css/1.less"
+import "./css/2.scss"
 
 const divEl = document.createElement("div");
 divEl.innerHTML = "Hello Webpack";
 divEl.className = "title"; 
 
+const divEl2 = document.createElement("div");
+divEl2.innerHTML = "webpack 是一个用于现代 JavaScript 应用程序的 静态模块打包工具。";
+divEl2.className = "content"; 
+
 document.body.appendChild(divEl)
+document.body.appendChild(divEl2)
