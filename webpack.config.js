fa3b8f8db10aa370d3f6e2a51bcd0e4fe8c8172e
xiaohh <1296522554@qq.com>
2022-03-07 01:08:11 +0800

    可以加载css文件了

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
     output: { // 出口文件
         path: path.resolve(__dirname, "./build"), // 存放目录
         filename: "bundle.js" // 打包之后文件的名字
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,  // 匹配所有以 .css 后缀结尾的文件
+                use: [
+                    {loader: "style-loader"},
+                    {loader: "css-loader"}
+                ]
+            }
+        ]
     }
 }
